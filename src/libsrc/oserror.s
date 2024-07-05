7ea3e5363e70ba15e557470fe0d8f5cebe9939b4
iss <iss@>
2024-07-05 19:33:28 +0300

    small fix in Makefile (and thest how PR works)

--- src/libsrc/oserror.s
+++ src/libsrc/oserror.s
@@ -16,6 +16,10 @@ EFATFS_START := 32
 
 .code
 
+; /!\ iss: this is quick quick hack to fix compilation /!\
+.export __mappederrno
+__mappederrno:
+
 ___osmaperrno:
     cmp #EFATFS_START
     bmi @L2
