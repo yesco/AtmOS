7ba0671c7352102c69a3d9da736278a625cca7a9
Sodiumlightbaby <edvard@xox.no>
2024-07-21 12:54:01 +0200

    Mouse wrap-around fix, add file selection mouse widgets

--- src/main.c
+++ src/main.c
@@ -37,9 +37,10 @@ char dir_buf[DIR_BUF_SIZE];
 char** dir_ptr_list = (char **)&dir_buf[DIR_BUF_SIZE];  //Reverse array
 unsigned int dir_entries;
 int dir_offset;
-char dir_txt[3];
+char dir_lpage[2];
+char dir_rpage[2];
 uint8_t dir_needs_refresh;
-#define DIR_PAGE_SIZE 23
+#define DIR_PAGE_SIZE 24
 
 #define PATH_SIZE 128
 char path[PATH_SIZE];
@@ -47,31 +48,31 @@ char path[PATH_SIZE];
 tui_widget ui[] = {
     { TUI_START, 1, 0, 0, 0 },
     //{ TUI_BOX,  39,28, 0, 0 },
-    { TUI_TXT,   1, 1, 40, txt_title },
-    { TUI_TXT,   1, 3,10, txt_mdisc }, { TUI_SEL, 12, 3, 6, txt_on },
-    { TUI_TXT,   3, 4, 4, txt_df0 }, { TUI_SEL,   8, 4,26, txt_empty },
-    { TUI_TXT,   3, 5, 4, txt_df1 }, { TUI_SEL,   8, 5,26, txt_empty },
-    { TUI_TXT,   3, 6, 4, txt_df2 }, { TUI_SEL,   8, 6,26, txt_empty },
-    { TUI_TXT,   3, 7, 4, txt_df3 }, { TUI_SEL,   8, 7,26, txt_empty },
-    { TUI_TXT,   1, 9,10, txt_tape },{ TUI_SEL, 12, 9, 6, txt_on },
-    { TUI_TXT,   3,10, 4, txt_tap }, { TUI_SEL,   8,10,18, txt_empty },
-    { TUI_TXT,   1,12,10, txt_mouse }, { TUI_SEL, 12,12, 6, txt_off },
-    { TUI_TXT,  32, 1, 7, txt_usb},
+    { TUI_TXT,   1, 0, 40, txt_title },
+    { TUI_TXT,   1, 2,10, txt_mdisc }, { TUI_SEL, 12, 2, 6, txt_on },
+    { TUI_TXT,   3, 3, 4, txt_df0 }, { TUI_SEL,   8, 3,26, txt_empty },
+    { TUI_TXT,   3, 4, 4, txt_df1 }, { TUI_SEL,   8, 4,26, txt_empty },
+    { TUI_TXT,   3, 5, 4, txt_df2 }, { TUI_SEL,   8, 5,26, txt_empty },
+    { TUI_TXT,   3, 6, 4, txt_df3 }, { TUI_SEL,   8, 6,26, txt_empty },
+    { TUI_TXT,   1, 8,10, txt_tape },{ TUI_SEL, 12, 8, 6, txt_on },
+    { TUI_TXT,   3, 9, 4, txt_tap }, { TUI_SEL,   8, 9,18, txt_empty },
+    { TUI_TXT,   1,11,10, txt_mouse }, { TUI_SEL, 12,11, 6, txt_off },
+    { TUI_TXT,  32, 0, 7, txt_usb},
 
-    { TUI_TXT,  29, 10, 1, txt_alt},
-    { TUI_SEL,  30, 10, 1, txt_rew},
-    { TUI_TXT,  32, 10, 3, txt_cnt},
-    { TUI_SEL,  36, 10, 1, txt_ffw},
+    { TUI_TXT,  29,  9, 1, txt_alt},
+    { TUI_SEL,  30,  9, 1, txt_rew},
+    { TUI_TXT,  32,  9, 3, txt_cnt},
+    { TUI_SEL,  36,  9, 1, txt_ffw},
+    { TUI_SEL,  35,  3, 2, txt_eject},
     { TUI_SEL,  35,  4, 2, txt_eject},
     { TUI_SEL,  35,  5, 2, txt_eject},
     { TUI_SEL,  35,  6, 2, txt_eject},
-    { TUI_SEL,  35,  7, 2, txt_eject},
-    { TUI_SEL,  38,  4, 1, txt_unlocked},
+    { TUI_SEL,  38,  3, 1, txt_unlocked},
+    { TUI_SEL,  38,  4, 1, txt_locked},
     { TUI_SEL,  38,  5, 1, txt_locked},
-    { TUI_SEL,  38,  6, 1, txt_locked},
-    { TUI_SEL,  38,  7, 1, txt_unlocked},
-    { TUI_TXT,  37, 10, 1, txt_alt},
-    { TUI_SEL,  38, 10, 1, txt_locked},
+    { TUI_SEL,  38,  6, 1, txt_unlocked},
+    { TUI_TXT,  37,  9, 1, txt_alt},
+    { TUI_SEL,  38,  9, 1, txt_locked},
     { TUI_END,   0, 0, 0, 0 }
 };
 #define IDX_FDC_ON 3
@@ -83,14 +84,19 @@ tui_widget ui[] = {
 #define IDX_TAP 15
 #define IDX_MOU_ON 17
 
-#define POPUP_FILE_START 4
+#define POPUP_FILE_START 6
 tui_widget popup[32] = {
-    { TUI_START, 3, 3, 0, 0 },
-    { TUI_BOX,  34,25, 0, 0 },
-    { TUI_TXT,   1, 0,32, path},
-    { TUI_TXT,  30,24, 2, dir_txt},
+    { TUI_START, 2, 2, 0, 0 },
+    { TUI_BOX,  35,26, 0, 0 },
+    { TUI_TXT,   1, 0,30, path},
+    { TUI_SEL,  31, 0, 3, txt_x},
+    { TUI_TXT,  30,25, 1, dir_lpage},
+    { TUI_TXT,  31,25, 1, dir_rpage},
     { TUI_END,   0, 0, 0, 0 }
 };
+#define IDX_XPAGE 3
+#define IDX_LPAGE 4
+#define IDX_RPAGE 5
 
 tui_widget warning[] = {
     { TUI_START, 4,10, 0, 0},
@@ -204,13 +210,17 @@ void parse_files_to_widget(void){
     popupf[i].len = 0;
     popupf[i].data = 0;
 
-    dir_txt[0] = '-';
-    dir_txt[1] = '-';
+    dir_lpage[0] = '-';
+    dir_rpage[0] = '-';
+    popup[IDX_LPAGE].type = TUI_TXT;
+    popup[IDX_RPAGE].type = TUI_TXT;
     if(dir_offset > 0){
-        dir_txt[0] = '<';
+        dir_lpage[0] = '<';
+        popup[IDX_LPAGE].type = TUI_SEL;
     }
     if(dir_offset+DIR_PAGE_SIZE < dir_entries){
-        dir_txt[1] = '>';
+        dir_rpage[0] = '>';
+        popup[IDX_RPAGE].type = TUI_SEL;
     }
     
 }
@@ -319,57 +329,78 @@ void DisplayKey(unsigned char key)
                         break;
                 }
             }else{
-                tmp_ptr = tui_get_data(tui_get_current());
-                if(tmp_ptr[0]=='/' || tmp_ptr[0]=='['){    //Directory or device selection
-                    if(tmp_ptr[0]=='['){
-                        path[0] = tmp_ptr[1];
-                        path[1] = tmp_ptr[2];
-                        path[2] = 0x00;
-                    }else if(tmp_ptr[1]=='.'){              //Go back down (/..)
-                        if((ret = strrchr(path,'/')) != NULL){
-                            ret[0] = 0x00;
-                        }else{
-                            path[0] = 0x00;
+                switch(tui_get_current()){
+                    //Control elements selected
+                    case(IDX_XPAGE):
+                        tui_clear_box(1);
+                        tui_draw(ui);
+                        tui_set_current(calling_widget);
+                        calling_widget = -1;
+                        break;
+                    case(IDX_LPAGE):
+                        dir_offset -= DIR_PAGE_SIZE;
+                        parse_files_to_widget();
+                        tui_draw(popup);
+                        break;
+                    case(IDX_RPAGE):
+                        dir_offset += DIR_PAGE_SIZE;
+                        parse_files_to_widget();
+                        tui_draw(popup);
+                        break;
+                    default:
+                        //Selection from the list
+                        tmp_ptr = tui_get_data(tui_get_current());
+                        if(tmp_ptr[0]=='/' || tmp_ptr[0]=='['){    //Directory or device selection
+                            if(tmp_ptr[0]=='['){
+                                path[0] = tmp_ptr[1];
+                                path[1] = tmp_ptr[2];
+                                path[2] = 0x00;
+                            }else if(tmp_ptr[1]=='.'){              //Go back down (/..)
+                                if((ret = strrchr(path,'/')) != NULL){
+                                    ret[0] = 0x00;
+                                }else{
+                                    path[0] = 0x00;
+                                }
+                            }else{
+                                strncat(path,tmp_ptr,128-strlen(path));
+                            }
+                            dir_needs_refresh = 1;
+                            dir_ok = dir_fill(path);
+                            parse_files_to_widget();
+                            tui_draw(popup);
+                            if(!dir_ok){
+                                tui_draw(warning);
+                            }
+                            break;
                         }
-                    }else{
-                        strncat(path,tmp_ptr,128-strlen(path));
-                    }
-                    dir_needs_refresh = 1;
-                    dir_ok = dir_fill(path);
-                    parse_files_to_widget();
-                    tui_draw(popup);
-                    if(!dir_ok){
-                        tui_draw(warning);
-                    }
-                    break;
+                        //File selection
+                        tmp_ptr = tmp_ptr + 1;      //adjust for leading space
+                        switch(calling_widget){
+                            case(IDX_DF0):
+                                drive = 0;
+                                break;
+                            case(IDX_DF1):
+                                drive = 1;
+                                break;
+                            case(IDX_DF2):
+                                drive = 2;
+                                break;
+                            case(IDX_DF3):
+                                drive = 3;
+                                break;
+                            case(IDX_TAP):
+                                drive = 4;
+                                break;
+                        }
+                        mount(drive,path,tmp_ptr);
+                        tui_clear_box(1);
+                        tui_draw(ui);
+                        tui_clear_txt(calling_widget);
+                        tui_set_data(calling_widget,tmp_ptr);
+                        tui_draw_widget(calling_widget);
+                        tui_set_current(calling_widget);
+                        calling_widget = -1;
                 }
-                //File selection
-                tmp_ptr = tmp_ptr + 1;      //adjust for leading space
-                switch(calling_widget){
-                    case(IDX_DF0):
-                        drive = 0;
-                        break;
-                    case(IDX_DF1):
-                        drive = 1;
-                        break;
-                    case(IDX_DF2):
-                        drive = 2;
-                        break;
-                    case(IDX_DF3):
-                        drive = 3;
-                        break;
-                    case(IDX_TAP):
-                        drive = 4;
-                        break;
-                }
-                mount(drive,path,tmp_ptr);
-                tui_clear_box(1);
-                tui_draw(ui);
-                tui_clear_txt(calling_widget);
-                tui_set_data(calling_widget,tmp_ptr);
-                tui_draw_widget(calling_widget);
-                tui_set_current(calling_widget);
-                calling_widget = -1;
             }
             break;
         case(KEY_RETURN):
@@ -408,32 +439,32 @@ void DisplayKey(unsigned char key)
 }
 
 unsigned char Mouse(unsigned char key){
-    static unsigned int prev_pos = 0;
-    static signed char sx = 0, sy = 0, prev_x = 0, prev_y = 0, prev_btn = 0;
-    static uint8_t cursor = 0; 
+    static uint16_t prev_pos = 0;
+    static int8_t sx = 0, sy = 0;
+    static uint8_t  prev_x = 0, prev_y = 0, prev_btn = 0, cursor = 0; 
     
-    unsigned int pos;
-    signed char x,y,btn;
+    uint16_t pos;
+    uint8_t x,y,btn;
     char *screen;
     uint8_t widget;
     
-    screen = (char*)(0xbb80);
+    screen = TUI_SCREEN;
     MIA.addr0 = 0x8000;
     MIA.step0 = 1;
     btn = MIA.rw0;
-    x = (MIA.rw0>>2);
-    y = (MIA.rw0>>2);
-    sx = sx + (x - prev_x);
-    sy = sy + (y - prev_y);
-    if(sx > 39)
-        sx = 39;
+    x = MIA.rw0;
+    y = MIA.rw0;
+    sx = sx + (((int8_t)(x - prev_x))>>3);
+    sy = sy + (((int8_t)(y - prev_y))>>3);
+    if(sx >= TUI_SCREEN_W)
+        sx = TUI_SCREEN_W-1;
     if(sx < 0)
         sx = 0;
-    if(sy > 24)
-        sy = 24;
+    if(sy >= TUI_SCREEN_H)
+        sy = TUI_SCREEN_H-1;
     if(sy < 0)
         sy = 0;
-    pos = (40*sy) + sx;
+    pos = (TUI_SCREEN_W * sy) + sx;
     if(pos != prev_pos){
         if(cursor)
             screen[prev_pos] ^= 0x80;
