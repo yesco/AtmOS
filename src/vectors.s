e0b77e0b1fa50a800dde27c9f78565f758e0aaa2
Sodiumlightbaby <edvard@xox.no>
2024-10-20 21:09:38 +0200

    Add Restore function to save and restore state of running Oric applications.

--- src/vectors.s
+++ src/vectors.s
@@ -4,11 +4,12 @@
 ;
 ; Defines the interrupt vector table.
 
-.import    _init
+.import    _init,  mia_save_state
 .import    _nmi_int, _irq_int
 
 .segment  "VECTORS"
 
-.addr      _nmi_int    ; NMI vector
+;.addr      _nmi_int    ; NMI vector
+.addr      mia_save_state; NMI vector
 .addr      _init       ; Reset vector
 .addr      _irq_int    ; IRQ/BRK vector
